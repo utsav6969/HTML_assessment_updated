c533d317c3afc4dacc017bbd2674ebe67ea1d3ac
Utsav Sharma <utsav.sharma@intelegencia.com>
2021-04-12 23:21:41 +0530

    In first design first image Image one should change every 5 seconds

--- Javascript/index.js
+++ Javascript/index.js
@@ -18,3 +18,27 @@
                  document.getElementById('image2').src=image3;
                  document.getElementById('image3').src=image2;
              }
+
+// Change Image One in every 5 seconds
+             var pictures = [], i = -1;
+             pictures[0] = "./images/slide1.png";
+             pictures[1] = "./images/slide2.png";
+             pictures[2] = "./images/slide3.png";
+             pictures[3] = "./images/slide4.png";
+             pictures[4] = "./images/slide5.png";
+         
+         
+         function Nextimage() 
+             {
+             if(i == pictures.length - 1) {
+               i = 0;
+             }
+             else{
+               i = i+1;
+             }
+             document.getElementById("image1").src = pictures[i];
+             }
+             function loadnextimage() 
+             {
+             setInterval(Nextimage, 5000);
+             }

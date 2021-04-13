82c78075a4f1c120a92c5478dc98332333256d83
Utsav Sharma <utsav.sharma@intelegencia.com>
2021-04-13 11:16:24 +0530

    In this assesment contact form is validated using jquery

--- Javascript/contact.js
+++ Javascript/contact.js
@@ -16,4 +16,24 @@ $(document).ready(function() {
             $(this).dequeue();
         });
     }
-});
+    $('#submit').click(function() {
+        var name = $('#fname').val();
+        var email = $('#email').val();
+        var comment = $('#comment').val();
+        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+        var namereg = /^[a-zA-Z ]{2,30}$/;
+
+        if(name == '' || name == null || email == '' || email == null || comment == '' || comment == null){
+            alert("All Fields are mandatory");
+            return false;
+        }
+        else if(!name.match(namereg)){
+            alert("Enter a valid name");
+            
+        }
+        else if(!email.match(emailReg)) {
+            alert("Enter a valid Email Address")
+
+        }
+    })
+    })
